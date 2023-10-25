b5a412983d8b653032ff0e7cf59be1dec6cb66f2
Siri Wang <siliwang.ico@gmail.com>
2023-10-24 21:17:04 -0400

    Add email and compant for admin view

--- apps/auth/admin_views.py
+++ apps/auth/admin_views.py
@@ -5,14 +5,14 @@ from flask import redirect, url_for, flash
 from .user import User, db
 
 class UserAdminView(ModelView):
-    column_list = ['id', 'username']  # Columns to display in the list view
-    form_columns = ['username', 'password']  # Fields to display in the add/edit forms
+    column_list = ['id', 'username', 'company', 'email']
+    form_columns = ['username', 'password', 'company', 'email']
+    column_searchable_list = ['username', 'company', 'email']  # Fields that can be searched
+    column_sortable_list = ['id', 'username', 'company', 'email']  # Fields that can be sorted
 
     def is_accessible(self):
-        # Ensure the user is authenticated and has admin privileges
         return current_user.is_authenticated and current_user.role == 'admin'
 
     def inaccessible_callback(self, name, **kwargs):
-        # Redirect unauthorized users to the login page
         flash('You do not have permission to access this page.')
         return redirect(url_for('auth.login'))
